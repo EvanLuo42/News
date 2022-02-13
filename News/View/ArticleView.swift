9bb6d77f1bb5434d5c18cee30464b67fc58cdb45
EvanLuo42 <ziyun.luo@icloud.com>
2022-02-13 21:38:02 +0800

    bug fix

---
+++ News/View/ArticleView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleView.swift
+//  News
+//
+//  Created by Evan Luo on 2022/2/13.
+//
+
+import SwiftUI
+
+struct ArticleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleView()
+    }
+}
