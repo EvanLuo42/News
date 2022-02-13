9f7d6fb87839d122773a838c9d2035476f8c8c12
EvanLuo42 <ziyun.luo@icloud.com>
2022-02-13 16:33:33 +0800

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Evan Luo on 2022/2/13.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
